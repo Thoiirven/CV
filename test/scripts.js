33b26f64e62aa7314535804182537c907d56d4d0
Thoiirven <christophe.marrel@gmail.com>
2023-11-27 14:34:13 +0100

    squelette portfolio bon

---
+++ test/scripts.js
@@ -0,0 +1,14 @@
+// Fonction pour ouvrir le pop-up avec l'image et la description
+function openPopup(imageSrc, title, description) {
+  document.getElementById('popupImg').src = imageSrc;
+  document.getElementById('popupDescription').innerHTML = `
+    <h3>${title}</h3>
+    <p>${description}</p>
+  `;
+  document.getElementById('popup').style.display = 'flex';
+}
+
+// Fonction pour fermer le pop-up
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
